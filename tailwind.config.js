316eec5e699be489e892d58e9961acdf3428d43b
Stefan Mohr <stefan.mohr@primoza.de>
2022-08-22 13:08:18 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./javascripts/**/*.{html,js,hbs}", "./common/**/*.html", "./common/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
