0d7fc16e1a212e6e328e5ac254006ce20bb891ea
Stefan Mohr <stefan.mohr@primoza.de>
2022-09-26 13:35:41 +0200

    style search banner

--- javascripts/discourse/api-initializers/init-search-banner.js
+++ javascripts/discourse/api-initializers/init-search-banner.js
@@ -128,6 +128,7 @@ export default apiInitializer("0.8", (api) => {
                         });
                 contents.push(h("div.search-button", searchButton));
             }
+            console.log(this);
 console.log(...corePanelContents.call(this));
             contents = contents.concat(...corePanelContents.call(this));
             console.log(contents);
