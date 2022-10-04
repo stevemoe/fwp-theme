003f1b75a23555ff0645ecae95a934b67089f223
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-04 16:13:06 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/components/categories-boxes.js
+++ javascripts/discourse/components/categories-boxes.js
@@ -3,33 +3,35 @@ import discourseComputed from "discourse-common/utils/decorators";
 import { equal } from "@ember/object/computed";
 import { isEmpty } from "@ember/utils";
 
-export default Component.extend({
-    tagName: "section",
-    classNameBindings: [
-        ":category-boxes",
-        "getInlineSVG",
-        "anyLogos:with-logos:no-logos",
-        "hasSubcategories:with-subcategories",
-    ],
-    noCategoryStyle: equal("siteSettings.category_style", "none"),
-    lockIcon: "lock",
+console.log("test category-boxes.js")
 
-    @discourseComputed("categories.[].uploaded_background.url")
-
-    // getInlineSVG() {
-    //     $.get(this.categories.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
-    //         console.log(data);
-    //     });
-    // },
-
-
-    @discourseComputed("categories.[].uploaded_logo.url")
-    anyLogos() {
-        return this.categories.any((c) => !isEmpty(c.get("uploaded_logo.url")));
-    },
-
-    @discourseComputed("categories.[].subcategories")
-    hasSubcategories() {
-        return this.categories.any((c) => !isEmpty(c.get("subcategories")));
-    },
-});
+// export default Component.extend({
+//     tagName: "section",
+//     classNameBindings: [
+//         ":category-boxes",
+//         "getInlineSVG",
+//         "anyLogos:with-logos:no-logos",
+//         "hasSubcategories:with-subcategories",
+//     ],
+//     noCategoryStyle: equal("siteSettings.category_style", "none"),
+//     lockIcon: "lock",
+//
+//     @discourseComputed("categories.[].uploaded_background.url")
+//
+//     getInlineSVG() {
+//         $.get(this.categories.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
+//             console.log(data);
+//         });
+//     },
+//
+//
+//     @discourseComputed("categories.[].uploaded_logo.url")
+//     anyLogos() {
+//         return this.categories.any((c) => !isEmpty(c.get("uploaded_logo.url")));
+//     },
+//
+//     @discourseComputed("categories.[].subcategories")
+//     hasSubcategories() {
+//         return this.categories.any((c) => !isEmpty(c.get("subcategories")));
+//     },
+// });
