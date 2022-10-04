1edda203186c035a14ad07f1ce025b55a195f71f
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-04 16:15:50 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/components/categories-boxes-test.js
+++ javascripts/discourse/components/categories-boxes-test.js
@@ -5,33 +5,33 @@ import { isEmpty } from "@ember/utils";
 
 console.log("test categories-boxes-test.js")
 
-// export default Component.extend({
-//     tagName: "section",
-//     classNameBindings: [
-//         ":category-boxes",
-//         "getInlineSVG",
-//         "anyLogos:with-logos:no-logos",
-//         "hasSubcategories:with-subcategories",
-//     ],
-//     noCategoryStyle: equal("siteSettings.category_style", "none"),
-//     lockIcon: "lock",
-//
-//     @discourseComputed("categories.[].uploaded_background.url")
-//
-//     getInlineSVG() {
-//         $.get(this.categories.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
-//             console.log(data);
-//         });
-//     },
-//
-//
-//     @discourseComputed("categories.[].uploaded_logo.url")
-//     anyLogos() {
-//         return this.categories.any((c) => !isEmpty(c.get("uploaded_logo.url")));
-//     },
-//
-//     @discourseComputed("categories.[].subcategories")
-//     hasSubcategories() {
-//         return this.categories.any((c) => !isEmpty(c.get("subcategories")));
-//     },
-// });
+export default Component.extend({
+    tagName: "section",
+    classNameBindings: [
+        ":category-boxes",
+        "getInlineSVG",
+        "anyLogos:with-logos:no-logos",
+        "hasSubcategories:with-subcategories",
+    ],
+    noCategoryStyle: equal("siteSettings.category_style", "none"),
+    lockIcon: "lock",
+
+    // @discourseComputed("categories.[].uploaded_background.url")
+    //
+    // getInlineSVG() {
+    //     $.get(this.categories.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
+    //         console.log(data);
+    //     });
+    // },
+    //
+    //
+    // @discourseComputed("categories.[].uploaded_logo.url")
+    // anyLogos() {
+    //     return this.categories.any((c) => !isEmpty(c.get("uploaded_logo.url")));
+    // },
+    //
+    // @discourseComputed("categories.[].subcategories")
+    // hasSubcategories() {
+    //     return this.categories.any((c) => !isEmpty(c.get("subcategories")));
+    // },
+});
