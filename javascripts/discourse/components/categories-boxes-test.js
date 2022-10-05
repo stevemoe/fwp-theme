c13d97446b45289a36e2b16bc85d2e0b4491b523
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-05 17:40:45 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/components/categories-boxes-test.js
+++ javascripts/discourse/components/categories-boxes-test.js
@@ -14,7 +14,7 @@ ajax("/site.json").then (function(result){ // Get list of categories
 });
 
 console.log("test categories-boxes-test.js");
-console.log(currentUser);
+console.log(this.currentUser);
 
 
 export default Component.extend({
