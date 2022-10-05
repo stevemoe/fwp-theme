fa617b717cb73763c660e1fa964b67c0ff94ac57
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-05 18:48:46 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/components/categories-boxes-test.js
+++ javascripts/discourse/components/categories-boxes-test.js
@@ -27,22 +27,22 @@ export default Component.extend({
     noCategoryStyle: equal("siteSettings.category_style", "none"),
     lockIcon: "lock",
 
-    // @discourseComputed("categories.[].uploaded_background.url")
-    //
-    // getInlineSVG() {
-    //     $.get(this.categories.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
-    //         console.log(data);
-    //     });
-    // },
-    //
-    //
-    // @discourseComputed("categories.[].uploaded_logo.url")
-    // anyLogos() {
-    //     return this.categories.any((c) => !isEmpty(c.get("uploaded_logo.url")));
-    // },
-    //
-    // @discourseComputed("categories.[].subcategories")
-    // hasSubcategories() {
-    //     return this.categories.any((c) => !isEmpty(c.get("subcategories")));
-    // },
+    @discourseComputed("categories.[].uploaded_background.url")
+
+    getInlineSVG() {
+        $.get(this.categoryName.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
+            console.log(data);
+        });
+    },
+
+
+    @discourseComputed("categories.[].uploaded_logo.url")
+    anyLogos() {
+        return this.categories.any((c) => !isEmpty(c.get("uploaded_logo.url")));
+    },
+
+    @discourseComputed("categories.[].subcategories")
+    hasSubcategories() {
+        return this.categories.any((c) => !isEmpty(c.get("subcategories")));
+    },
 });
