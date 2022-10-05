f1731f97dc6842386aa79980fa711106faed1dc1
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-05 17:10:38 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/api-initializers/init-categories-in-bubbles.js
+++ javascripts/discourse/api-initializers/init-categories-in-bubbles.js
@@ -1,9 +1,16 @@
 import {createWidget} from 'discourse/widgets/widget';
+import {apiInitializer} from "discourse/lib/api";
 
-createWidget('categories-in-bubbles', {
-    tagName: "div.categories-in-bubbles",
 
-    html() {
-        return "categories"
-    }
+
+export default apiInitializer("0.8", (api) => {
+
+    createWidget('categories-in-bubbles', {
+        tagName: "div.categories-in-bubbles",
+
+        html() {
+            return "categories"
+        }
+    });
+
 });
