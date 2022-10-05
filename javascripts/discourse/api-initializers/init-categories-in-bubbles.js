70ffa95eaf87a15dbfd3026cd690316d36d67a68
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-05 18:33:05 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/api-initializers/init-categories-in-bubbles.js
+++ javascripts/discourse/api-initializers/init-categories-in-bubbles.js
@@ -5,7 +5,7 @@ import {apiInitializer} from "discourse/lib/api";
 
 export default apiInitializer("0.8", (api) => {
 
-    createWidget('categories-in-bubbles.hbs', {
+    createWidget('categories-in-bubbles', {
         tagName: "div.categories-in-bubbles.hbs",
 
 
