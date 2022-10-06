db2da592ca63441263f088421088dccb7a907a7c
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-06 16:34:29 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/api-initializers/init-categories-in-bubbles.js
+++ javascripts/discourse/api-initializers/init-categories-in-bubbles.js
@@ -4,13 +4,13 @@ import {apiInitializer} from "discourse/lib/api";
 
 export default apiInitializer("0.8", (api) => {
 
-    ajax("/site.json").then (function(result){ // Get list of categories
-        let categoryName = [];
-        result.categories.forEach(function(categories){
-            categoryName.push(categories);
-        });
-        component.set('categoryName', categoryName);
-    });
+    // ajax("/site.json").then (function(result){ // Get list of categories
+    //     let categoryName = [];
+    //     result.categories.forEach(function(categories){
+    //         categoryName.push(categories);
+    //     });
+    //     component.set('categoryName', categoryName);
+    // });
 
     createWidget('categories-in-bubbles', {
         tagName: "div.categories-in-bubbles",
