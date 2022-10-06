944147eb1013b725a6136bfffebea1d4981f0623
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-06 16:32:54 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/components/categories-in-bubbles.js
+++ javascripts/discourse/components/categories-in-bubbles.js
@@ -1,14 +1,16 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
-import {equal} from "@ember/object/computed";
-import {isEmpty} from "@ember/utils";
+import { equal } from "@ember/object/computed";
+import { isEmpty } from "@ember/utils";
 
+@discourseComputed("categories.[].uploaded_logo.url")
+console.log(this.categories);
 
 export default Component.extend({
-    tagName: "h1",
+    tagName: "section",
     classNameBindings: [
         ":category-bubbles",
-        "getInlineSVG",
+        "console",
         "anyLogos:with-logos:no-logos",
         "hasSubcategories:with-subcategories",
     ],
@@ -16,7 +18,19 @@ export default Component.extend({
     lockIcon: "lock",
 
 
+    @discourseComputed("categories.[].uploaded_logo.url")
+    console() {
 
+        console.log(this.categories);
+    },
+    // @discourseComputed("categories.[].uploaded_background.url")
+    //
+    // getInlineSVG() {
+    //     $.get(this.categoryName.any((c) => !isEmpty(c.get("uploaded_background.url"))),function(data) {
+    //         console.log(data);
+    //     });
+    // },
+    //
     //
     // @discourseComputed("categories.[].uploaded_logo.url")
     // anyLogos() {
