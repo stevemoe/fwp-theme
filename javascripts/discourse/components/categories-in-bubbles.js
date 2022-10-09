eb6ea1f782add9fc758219c70ac1ff91fd1baa93
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-09 14:04:48 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/components/categories-in-bubbles.js
+++ javascripts/discourse/components/categories-in-bubbles.js
@@ -7,7 +7,7 @@ export default Component.extend({
     tagName: "section",
     classNameBindings: [
         ":category-bubbles",
-        "firstCategory",
+        ":firstCategory",
         "anyLogos:with-logos:no-logos",
         "hasSubcategories:with-subcategories",
     ],
