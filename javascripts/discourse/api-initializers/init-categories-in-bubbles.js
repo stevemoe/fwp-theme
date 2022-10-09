e72a78949269d869a5101c184907e16fab26a3b3
Stefan Mohr <stefan.mohr@primoza.de>
2022-10-09 12:51:27 +0200

    category boxes svg as CdnImg

--- javascripts/discourse/api-initializers/init-categories-in-bubbles.js
+++ javascripts/discourse/api-initializers/init-categories-in-bubbles.js
@@ -10,28 +10,30 @@ export default apiInitializer("0.8", (api) => {
 
 
         html() {
+            const container = Discourse.__container__;
+            const categories = container.lookup("controller:navigation/categories").site.categories;
+            return categories;
 
-
-            const ajax = require('discourse/lib/ajax').ajax;
-            let categoryList =[];
-            ajax("/site.json").then(function (result) { // Get list of categories
-                result.categories.forEach(function (categories) {
-                    categoryList.push(categories);
-                }).catch(e => {
-                    console.log(e);
-                });;
-                console.log("category1");
-                console.log(categoryList);
-            });
-            console.log("category2");
-            console.log(categoryList.firstObject);
-
-            categoryList.forEach(function (category) {
-                console.log(category)
-            });
-            console.log(this.get("model"));
-
-            return (category1);
+            // const ajax = require('discourse/lib/ajax').ajax;
+            // let categoryList =[];
+            // ajax("/site.json").then(function (result) { // Get list of categories
+            //     result.categories.forEach(function (categories) {
+            //         categoryList.push(categories);
+            //     }).catch(e => {
+            //         console.log(e);
+            //     });;
+            //     console.log("category1");
+            //     console.log(categoryList);
+            // });
+            // console.log("category2");
+            // console.log(categoryList.firstObject);
+            //
+            // categoryList.forEach(function (category) {
+            //     console.log(category)
+            // });
+            // console.log(this.get("model"));
+            //
+            // return (category1);
         }
     });
 
